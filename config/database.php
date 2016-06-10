bb1c7dc654c38a9c6f6c147329cbbb894223f8b3
Odirlei Borgert <odirleiborgert@gmail.com>
2016-06-09 22:08:40 -0300

    Adicionado configurações travis ci

--- config/database.php
+++ config/database.php
@@ -78,6 +78,18 @@ return [
             'schema' => 'public',
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'username'  => env('DB_TEST_USERNAME', 'homestead'),
+            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
     ],
 
     /*

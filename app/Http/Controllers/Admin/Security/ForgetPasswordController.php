4753cb42508030634f674b46ad2b8dc0328a540b
Odirlei Borgert <odirleiborgert@gmail.com>
2016-06-16 21:53:31 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/Admin/Security/ForgetPasswordController.php
+++ app/Http/Controllers/Admin/Security/ForgetPasswordController.php
@@ -8,6 +8,6 @@ class ForgetPasswordController extends Controller
 {
     public function index()
     {
-		return view('admin.security.forget-password');
+        return view('admin.security.forget-password');
     }
 }
--- app/Http/Routes/Admin/mailbox.php
+++ app/Http/Routes/Admin/mailbox.php
@@ -3,37 +3,37 @@
 // Mailbox
 Route::group(['prefix' => 'mailbox', 'as' => 'mailbox.'], function () {
     Route::get('inbox', [
-        'as' => 'inbox', 
-        'uses' => 'Admin\Mailbox\InboxController@index'
+        'as' => 'inbox',
+        'uses' => 'Admin\Mailbox\InboxController@index',
     ]);
 
     Route::get('trash', [
-        'as' => 'trash', 
-        'uses' => 'Admin\Mailbox\TrashController@index'
+        'as' => 'trash',
+        'uses' => 'Admin\Mailbox\TrashController@index',
     ]);
 
     Route::post('trash/{id}', [
-        'as' => 'trash_in', 
-        'uses' => 'Admin\Mailbox\TrashController@trash'
+        'as' => 'trash_in',
+        'uses' => 'Admin\Mailbox\TrashController@trash',
     ])->where('id', '[0-9]+');
 
     Route::get('archive', [
-        'as' => 'archive', 
-        'uses' => 'Admin\Mailbox\ArchiveController@index'
+        'as' => 'archive',
+        'uses' => 'Admin\Mailbox\ArchiveController@index',
     ]);
 
     Route::post('archive/{id}', [
-        'as' => 'archive_in', 
-        'uses' => 'Admin\Mailbox\ArchiveController@archive'
+        'as' => 'archive_in',
+        'uses' => 'Admin\Mailbox\ArchiveController@archive',
     ])->where('id', '[0-9]+');
 
     Route::get('message/{id}', [
-        'as' => 'message', 
-        'uses' => 'Admin\Mailbox\MessageController@index'
+        'as' => 'message',
+        'uses' => 'Admin\Mailbox\MessageController@index',
     ])->where('id', '[0-9]+');
 
     Route::post('delete', [
-        'as' => 'delete', 
-        'uses' => 'Admin\Mailbox\DeleteController@destroy'
+        'as' => 'delete',
+        'uses' => 'Admin\Mailbox\DeleteController@destroy',
     ]);
 });

57dce36ed00d9846f052b5a271ac018cb122ee89
Odirlei Borgert <odirleiborgert@gmail.com>
2016-09-17 22:01:24 -0300

    Fixed type number input order

--- resources/views/admin/pages/contents/edit.blade.php
+++ resources/views/admin/pages/contents/edit.blade.php
@@ -46,7 +46,7 @@
                             </div>
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">@lang('admin/_globals.forms.order'):</label>
-                                <div class="col-sm-10"><input type="integer" min="0" name="order" class="form-control" value="{{ $content->order }}"></div>
+                                <div class="col-sm-10"><input type="number" min="0" name="order" class="form-control" value="{{ $content->order }}"></div>
                             </div>
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">@lang('admin/_globals.forms.status'):</label>

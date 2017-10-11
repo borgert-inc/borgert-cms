4b87310c191409c121eba6094a138f23201cf939
Odirlei Borgert <odirleiborgert@gmail.com>
2017-10-11 13:52:18 -0300

    Upgrade Laravel 5.5 issue #75

--- config/borgert.php
+++ config/borgert.php
@@ -9,8 +9,8 @@ return [
     */
 
     'name' => 'Borgert CMS',
-    'laravel' => 'Laravel 5.4',
-    'version' => '0.0.32',
+    'laravel' => 'Laravel 5.5',
+    'version' => '1.0.0',
     'opensource' => 'https://github.com/odirleiborgert/borgert-cms',
 
 ];
